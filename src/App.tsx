a7431a73c8d8821f6492501095d387957d7e3f7d
Poostoi <oleg.nour@bk.ru>
2022-05-14 11:55:51 +0300

    checking the connection with the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
+	useEffect(() => {
+		const url = 'https://localhost:7019/api/Account';
+		/* axios({
+			method: 'post',
+			url: url + '/Registration',
+			data: {
+				"login": "string1",
+				"password": "string"
+			}
+		}).then(responce => console.log(responce)); */
+		axios({
+			method: 'post',
+			url: url + '/Login',
+			data: {
+				"login": "string1",
+				"password": "string"
+			}
+		}).then(responce => console.log(responce));
+	})
 	return (
 		<div className="App">
 
@@ -9,4 +29,4 @@ function App() {
 	);
 }
 
-export default App;
+
